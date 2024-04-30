16c59b0cd6c84822cc9d34757e467d84a6dc37d5
AdityapalW <146784567+AdityapalW@users.noreply.github.com>
2024-04-30 13:57:28 -0700

    Initial Commit

---
+++ sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by Adityapal Waraich on 2024-04-30.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
